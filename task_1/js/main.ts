96b8abb1ced358d9a4ef6d72fa1293e528c31891
Ahmed Sami <ahmedssami.dev@gmail.com>
2025-07-20 18:08:59 +0300

    Printing teachers

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -11,9 +11,20 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
   lastName: "Doe",
   location: "London",
+};
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
